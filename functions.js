9bb2c5f8a26d7a2eca0e762811f9f98df44bdfc9
Blanche Haddad <blancherachel@gmail.com>
2022-09-22 13:30:44 -0600

    Save work so far

--- functions.js
+++ functions.js
@@ -1,10 +1,32 @@
-// 1: Write a function named printGreeting that prints a simple greeting message, the same one, every time it is called. Call this function 3 times.
+// 1: Write a function named printGreeting that prints a simple greeting message, the same one,
+//every time it is called. Call this function 3 times.
+function printGreeting(){
+  console.log("Hello fellow Mod 0 student!");
+}
+printGreeting();
+printGreeting();
+printGreeting();
 
 
-// 2: Write a function that accepts 1 argument. The function should console.log a sentence that interpolates the data passed in.
+// 2: Write a function that accepts 1 argument. The function should console.log a sentence that
+//interpolates the data passed in.
+function printPace(pace1){
+  console.log(`On my run today my pace was ${pace1}.`);
+}
+printPace("9:18");
 
 
-// 3: Write a function that has 3 parameters: a string and two numbers. The String will be the name of a company, and the numbers will represent the minimum and maximum of a pay range for a posted job. The function should print out a sentence that includes the name of the company and the range itself (if the numbers passed in are 90000 and 110000, the pay range is 20000).
+// 3: Write a function that has 3 parameters: a string and two numbers.
+//The String will be the name of a company, and the numbers will represent the
+//minimum and maximum of a pay range for a posted job.
+//The function should print out a sentence that includes the name of the company
+//and the range itself (if the numbers passed in are 90000 and 110000, the pay range is 20000).
+
+function printPayRange(name, lowest, highest){
+  var range = highest - lowest
+console.log(`The compensation for this job at ${name} has a range of ${range}`);
+}
+printPayRange("Google", 110000, 150000);
 
 
 // 4: Write a function that satifies the following interaction pattern:
